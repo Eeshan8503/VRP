af5b8a4fd944e6a52ee9e843ce48efcfb1fc6bc0
Arnish-B <arnish.2020@vitstudent.ac.in>
2023-05-24 02:51:09 +0530

    Northwest

--- src/Northwest.java
+++ src/Northwest.java
@@ -21,14 +21,22 @@ public class Northwest {
 //            reiterate until cpu boundaries go out of range
 
             int i=0,j=0;
-//            while(i<n && j<n){
-//                if(supply[i])
-//            }
+            while(i<n && j<n){
+                if(supply[i] > demand[j]){
+                    supply[j] = supply[i] - demand[j];
+                    matrix[i][j].setAllocation(demand[j]);
+                    ++i;
+                    supply[i] = 0;
+                    demand[j] = 0;
 
-
-
-
-
+                }
+                else{
+                    demand[j] = demand[j] - supply[i];
+                    matrix[i][j].setAllocation(supply[i]);
+                    ++j;
+                    supply[i] = 0;
+                }
+            }
             return true;
         }
         catch (Exception e){
