1d3a3df2b4e89412725d21d1e51f3c27fde735ac
Eeshan8503 <eeshanmattey@gmail.com>
2023-05-24 04:14:24 +0530

    northwest

--- src/App.java
+++ src/App.java
@@ -6,20 +6,31 @@ class CostPerUnitCell{
     long allocation;
     double cpu;
     public CostPerUnitCell(){}
-    public CostPerUnitCell(int allocation, int cpu){
+    public CostPerUnitCell(long allocation, double cpu){
         this.allocation = allocation;
         this.cpu = cpu;
     }
 
-    public void setAllocation(long allocation) {
-        this.allocation = allocation;
+    public void setAllocation(long alloc) {
+
+        System.out.println("Allocating func called");
+        this.allocation = alloc;
+        System.out.println(this.allocation);
     }
 
 
 
 }
 public class App {
-
+    public static double compute(CostPerUnitCell[][] matrix){
+        double result=0;
+        for(CostPerUnitCell i[]:matrix){
+            for(CostPerUnitCell j:i){
+                result+=j.cpu*j.allocation;
+            }
+        }
+        return result;
+    }
     public double computeScalar(double petrolPrice, double initial_mileage, double extraLoad){
         double mileage = initial_mileage - ((extraLoad/45.359)*0.033);
         return petrolPrice/mileage;
@@ -27,49 +38,64 @@ public class App {
     public static void main(String[] args){
         // Visualization code goes here -> receive the distance matrix from the frontend
 
-        long[][] distanceMatrix = {
-                {0, 548, 776, 696, 582, 274, 502, 194, 308, 194, 536, 502, 388, 354, 468, 776, 662},
-                {548, 0, 684, 308, 194, 502, 730, 354, 696, 742, 1084, 594, 480, 674, 1016, 868, 1210},
-                {776, 684, 0, 992, 878, 502, 274, 810, 468, 742, 400, 1278, 1164, 1130, 788, 1552, 754},
-                {696, 308, 992, 0, 114, 650, 878, 502, 844, 890, 1232, 514, 628, 822, 1164, 560, 1358},
-                {582, 194, 878, 114, 0, 536, 764, 388, 730, 776, 1118, 400, 514, 708, 1050, 674, 1244},
-                {274, 502, 502, 650, 536, 0, 228, 308, 194, 240, 582, 776, 662, 628, 514, 1050, 708},
-                {502, 730, 274, 878, 764, 228, 0, 536, 194, 468, 354, 1004, 890, 856, 514, 1278, 480},
-                {194, 354, 810, 502, 388, 308, 536, 0, 342, 388, 730, 468, 354, 320, 662, 742, 856},
-                {308, 696, 468, 844, 730, 194, 194, 342, 0, 274, 388, 810, 696, 662, 320, 1084, 514},
-                {194, 742, 742, 890, 776, 240, 468, 388, 274, 0, 342, 536, 422, 388, 274, 810, 468},
-                {536, 1084, 400, 1232, 1118, 582, 354, 730, 388, 342, 0, 878, 764, 730, 388, 1152, 354},
-                {502, 594, 1278, 514, 400, 776, 1004, 468, 810, 536, 878, 0, 114, 308, 650, 274, 844},
-                {388, 480, 1164, 628, 514, 662, 890, 354, 696, 422, 764, 114, 0, 194, 536, 388, 730},
-                {354, 674, 1130, 822, 708, 628, 856, 320, 662, 388, 730, 308, 194, 0, 342, 422, 536},
-                {468, 1016, 788, 1164, 1050, 514, 514, 662, 320, 274, 388, 650, 536, 342, 0, 764, 194},
-                {776, 868, 1552, 560, 674, 1050, 1278, 742, 1084, 810, 1152, 274, 388, 422, 764, 0, 798},
-                {662, 1210, 754, 1358, 1244, 708, 480, 856, 514, 468, 354, 844, 730, 536, 194, 798, 0},
-        };
+//        long[][] distanceMatrix = {
+//                {0, 548, 776, 696, 582, 274, 502, 194, 308, 194, 536, 502, 388, 354, 468, 776, 662},
+//                {548, 0, 684, 308, 194, 502, 730, 354, 696, 742, 1084, 594, 480, 674, 1016, 868, 1210},
+//                {776, 684, 0, 992, 878, 502, 274, 810, 468, 742, 400, 1278, 1164, 1130, 788, 1552, 754},
+//                {696, 308, 992, 0, 114, 650, 878, 502, 844, 890, 1232, 514, 628, 822, 1164, 560, 1358},
+//                {582, 194, 878, 114, 0, 536, 764, 388, 730, 776, 1118, 400, 514, 708, 1050, 674, 1244},
+//                {274, 502, 502, 650, 536, 0, 228, 308, 194, 240, 582, 776, 662, 628, 514, 1050, 708},
+//                {502, 730, 274, 878, 764, 228, 0, 536, 194, 468, 354, 1004, 890, 856, 514, 1278, 480},
+//                {194, 354, 810, 502, 388, 308, 536, 0, 342, 388, 730, 468, 354, 320, 662, 742, 856},
+//                {308, 696, 468, 844, 730, 194, 194, 342, 0, 274, 388, 810, 696, 662, 320, 1084, 514},
+//                {194, 742, 742, 890, 776, 240, 468, 388, 274, 0, 342, 536, 422, 388, 274, 810, 468},
+//                {536, 1084, 400, 1232, 1118, 582, 354, 730, 388, 342, 0, 878, 764, 730, 388, 1152, 354},
+//                {502, 594, 1278, 514, 400, 776, 1004, 468, 810, 536, 878, 0, 114, 308, 650, 274, 844},
+//                {388, 480, 1164, 628, 514, 662, 890, 354, 696, 422, 764, 114, 0, 194, 536, 388, 730},
+//                {354, 674, 1130, 822, 708, 628, 856, 320, 662, 388, 730, 308, 194, 0, 342, 422, 536},
+//                {468, 1016, 788, 1164, 1050, 514, 514, 662, 320, 274, 388, 650, 536, 342, 0, 764, 194},
+//                {776, 868, 1552, 560, 674, 1050, 1278, 742, 1084, 810, 1152, 274, 388, 422, 764, 0, 798},
+//                {662, 1210, 754, 1358, 1244, 708, 480, 856, 514, 468, 354, 844, 730, 536, 194, 798, 0},
+//        };
 
         double[][] distanceMatrix = {
-                {0,3,4,5,7,8},
-                {3,0,6,4,4,6},
-                {4,6,0,6,7,7},
-                {5,4,6,0,5,9},
-                {5,4,7,5,0,7},
-                {8,6,7,9,7,0}
+                {0, 3, 4, 5, 7, 8},
+                {3, 0, 6, 4, 4, 6},
+                {4, 6, 0, 6, 7, 7},
+                {5, 4, 6, 0, 5, 9},
+                {5, 4, 7, 5, 0, 7},
+                {8, 6, 7, 9, 7, 0}
         };
+        double petrolPrice = 100;
+        double initial_mileage = 17;
+        double extraLoad = 140;
+
+        double scalar = new App().computeScalar(petrolPrice, initial_mileage, extraLoad);
+
         int n = distanceMatrix.length;
+        long[] supply={70,30,50,0,0,0};
+        long[] demand={0,0,0,65,42,43};
         // Compute the cost per unit matrix
         CostPerUnitCell[][] matrix = new CostPerUnitCell[n][n];
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 // Calculate CPU using distance matrix and the scalar
-                matrix[i][j] = new CostPerUnitCell(0,0);
+                matrix[i][j] = new CostPerUnitCell(0, 1 * distanceMatrix[i][j]);
             }
         }
-        if(n < 7){
+        Debugger debug=new Debugger();
+//        debug.Cpu_printer(matrix);
+        if (n < 7) {
             // Northwest Corner method multiple times and compare resutls ( bruteforce )
-        }
-        else{
+            Northwest obj=new Northwest(n,matrix,supply,demand);
+            obj.compute();
+            System.out.println("===================================================");
+            debug.Cpu_printer(matrix);
+            System.out.println(compute(matrix));
+        } else {
             // Northwest Corner method followed by (n-1)*(n-1) times MODI method [ or if lowest cost found before that ]
         }
+
     }
 }
--- src/Northwest.java
+++ src/Northwest.java
@@ -1,5 +1,7 @@
 package src;
 
+import java.util.Arrays;
+
 public class Northwest {
     long n;
     CostPerUnitCell[][] matrix;
@@ -18,24 +20,29 @@ public class Northwest {
 //            find least of supply and demand
 //            update cpu boundaries
 //            reiterate until cpu boundaries go out of range
-
+            Debugger debug=new Debugger();
             int i=0,j=0;
+            System.out.println(Arrays.toString(supply));
+            System.out.println(Arrays.toString(demand));
             while(i<n && j<n){
-                if(supply[i] > demand[j]){
-                    supply[j] = supply[i] - demand[j];
+                debug.Cpu_printer(matrix);
+                System.out.println("supply: "+supply[i]+" demand: "+demand[j]);
+                if(demand[j]<supply[i]){
                     matrix[i][j].setAllocation(demand[j]);
-                    ++i;
-                    supply[i] = 0;
-                    demand[j] = 0;
+                    supply[j]=supply[i]-demand[j];
+                    supply[i]=0;
+                    j++;
 
                 }
                 else{
-                    demand[j] = demand[j] - supply[i];
                     matrix[i][j].setAllocation(supply[i]);
-                    ++j;
-                    supply[i] = 0;
+                    demand[j]=demand[j]-supply[i];
+
+                    i++;
                 }
             }
+            System.out.println(Arrays.toString(supply));
+            System.out.println(Arrays.toString(demand));
             return true;
         }
         catch (Exception e){
