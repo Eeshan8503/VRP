897e578ff9c1f8eb6b60939077cc48bc62179f0a
Arnish-B <arnish.2020@vitstudent.ac.in>
2023-05-24 21:46:45 +0530

    modularised further

--- src/App.java
+++ src/App.java
@@ -2,15 +2,7 @@ package src;
 
 import java.lang.*;
 
-class CostPerUnitCell{
-    double allocation;
-    double cpu;
-    public CostPerUnitCell(){}
-    public CostPerUnitCell(double allocation, double cpu){
-        this.allocation = allocation;
-        this.cpu = cpu;
-    }
-}
+
 public class App {
     public static double compute(CostPerUnitCell[][] matrix){
         double result=0;
