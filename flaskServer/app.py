b52e6740245ac183bf55a07d2e7796c2d7528513
Eeshan8503 <eeshanmattey@gmail.com>
2023-06-28 01:37:16 +0530

    cesium_demo

--- flaskServer/app.py
+++ flaskServer/app.py
@@ -1,17 +1,23 @@
+import veroviz as vrv
+import os
+import pandas as pd
+import numpy as np
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+import codeing
+import cesium_demo_python
 app = Flask(__name__)
 CORS(app)
-
+data=5
 @app.route('/init-data', methods=['POST'])
 def init_data():
     data = request.get_json()
-    print('Received data:', data)
+    # print('Received data:', data)
     response = {
         'message': 'Data received successfully',
-        'data': data
     }
+    data=codeing.extracter(data);
+    cesium_demo_python.runner(data)
     return jsonify(response)
 
 if __name__ == '__main__':
